9847aaa4e21ec4650d6be15c40ebd8a9b7515617
Alessandra <alessandraaquino2003@gmail.com>
2023-09-15 17:39:10 -0300

    atualizando

--- finecap/form.py
+++ finecap/form.py
@@ -11,10 +11,10 @@ class ReservaForm(ModelForm):
         fields = '__all__'
       
         widgets = {
-            'nome' : forms.TextInput(attrs={'class': 'form-control' }),
+            'nome' : forms.TextInput(attrs={'class': 'form-control'}),
             'cnpj' : forms.TextInput(attrs={'class': 'form-control' }),
             'nome_empresa' : forms.TextInput(attrs={'class': 'form-control' }),
             'categoria_empresa' : forms.TextInput(attrs={'class': 'form-control' }),
-            'quitado' : forms.NullBooleanSelect(attrs={'class': 'form-control'}),
+            #'quitado' : forms.BooleanField(),
             'stand' : forms.Select(attrs={'class': 'form-control'}),
         }
